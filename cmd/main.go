04f18244d98769aadb085ed8fc7fc899edfbf992
Abdulaziz Nal <abdulaziz.nal@anywai.ch>
2024-09-22 16:29:58 +0300

    replace implementation with contacts form and list

--- cmd/main.go
+++ cmd/main.go
@@ -22,25 +22,51 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 	return t.tmpl.ExecuteTemplate(w, name, data)
 }
 
-type Count struct {
-	Count int
+type Contact struct {
+	Name  string
+	Email string
+}
+
+type Contacts = []Contact
+
+type ContactData struct {
+	Contacts Contacts
+}
+
+func createNewData() ContactData {
+	return ContactData{
+		Contacts: []Contact{
+			createNewContact("John Doe", "johndoe@gmail.com"),
+			createNewContact("Isopropyl Alcohol", "ipa@gmail.com"),
+		},
+	}
+}
+
+func createNewContact(name, email string) Contact {
+	return Contact{
+		Name:  name,
+		Email: email,
+	}
 }
 
 func main() {
 	e := echo.New()
 
-	count := Count{Count: 0}
+	contacts := createNewData()
 
 	e.Renderer = newTemplate()
 	e.Use(middleware.Logger())
 
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", count)
+		return c.Render(200, "index", contacts)
 	})
 
-	e.POST("/count", func(c echo.Context) error {
-		count.Count++
-		return c.Render(200, "count", count)
+	e.POST("/contacts", func(c echo.Context) error {
+		name, email := c.FormValue("name"), c.FormValue("email")
+
+		contacts.Contacts = append(contacts.Contacts, createNewContact(name, email))
+
+		return c.Render(200, "contacts-list", contacts)
 	})
 
 	e.Logger.Fatal(e.Start(":3001"))
